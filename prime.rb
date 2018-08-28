89167f8d5c9773006cb921196adf1596a2986911
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-28 19:00:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 # Add  code here!
 def prime?(array)
   num=2
-  while i
+
   array.each do |i|
-  if i===2 
+  while i>num
+    if i===2 
     return true
-  elsif i%num==0
+  
+    if i%num==0
     return true
     num+=1

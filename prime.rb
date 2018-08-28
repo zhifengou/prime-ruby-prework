e232051a2d9d778e857552178fdaf79fb1499318
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-28 19:15:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(i)
+  num=2
+  if i<=1 
+    return false
+  end
+  while num<i
+    if i===2 
+    return true
+  end
+    if i%num==0
+    return false
+   end 
+    num+=1
+  end
+  return true
+end
